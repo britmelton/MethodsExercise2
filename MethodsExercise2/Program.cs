f2c650a0de7758a8bda818c415d03a040b53dd64
brittany melton <brittanymelton26@gmail.com>
2022-01-10 15:52:29 -0500

    Methods Exercise 2

---
+++ MethodsExercise2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace MethodsExercise2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Add(3, 6));
+            Console.WriteLine(Subtract(8, 4));
+            Console.WriteLine(Multiply(12, 24));
+            Console.WriteLine(Divide(12, 6));
+            Console.WriteLine(Modulus(25, 6));
+
+            
+
+        }
+
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static int Subtract(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        public static int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public static int Divide(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+
+        public static int Modulus(int num1, int num2)
+        {
+            return num1 % num2;
+        }
+        
+    }
+}
