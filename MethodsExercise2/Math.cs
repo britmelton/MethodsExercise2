b487faa19f2c5fb9d660b1ca6a253b005eb6481d
brittany melton <brittanymelton26@gmail.com>
2022-02-28 16:03:22 -0500

    added Math class and comments

--- MethodsExercise2/Math.cs
+++ MethodsExercise2/Math.cs
@@ -6,7 +6,42 @@ using System.Threading.Tasks;
 
 namespace MethodsExercise2
 {
-    internal class Math
+    public class Math
     {
+        //Write different methods that use each math operator but allow you to plug in different integers. 
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public static int Subtract(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+        public static int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+        public static int Divide(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+        public static int Modulus(int num1, int num2)
+        {
+            return num1 % num2;
+        }
+
+        //Challenge mode: Change the functions to use the params keyword to support a range of arguments.
+
+        public static int Sum(params int[] nums)
+        {
+            var sum = 0;
+            foreach (int num in nums)
+            {
+                sum += num;
+            }
+            return sum;
+        }
+
+
     }
 }
--- MethodsExercise2/Program.cs
+++ MethodsExercise2/Program.cs
@@ -6,57 +6,25 @@ namespace MethodsExercise2
     {
         static void Main(string[] args)
         {
-            #region writelines
-            /*Console.WriteLine(Add(3, 6));
-              Console.WriteLine(Subtract(8, 4));
-              Console.WriteLine(Multiply(12, 24));
-              Console.WriteLine(Divide(12, 6));
-              Console.WriteLine(Modulus(25, 6));*/
-            #endregion
 
 
-            Console.WriteLine(Sum(2, 5, 7, 8));
+            //In class exercise to practice methods
+
+            Console.WriteLine(Math.Add(3, 6));
+            Console.WriteLine(Math.Subtract(8, 4));
+            Console.WriteLine(Math.Multiply(12, 24));
+            Console.WriteLine(Math.Divide(12, 6));
+            Console.WriteLine(Math.Modulus(25, 6));
+          
+
+
+            Console.WriteLine(Math.Sum(2, 5, 7, 8));
 
 
 
         }
-        #region calculate methods1
-        /*public static int Add(int num1, int num2)
-        {
-            return num1 + num2;
-        }
-
-        public static int Subtract(int num1, int num2)
-        {
-            return num1 - num2;
-        }
-
-        public static int Multiply(int num1, int num2)
-        {
-            return num1 * num2;
-        }
-
-        public static int Divide(int num1, int num2)
-        {
-            return num1 / num2;
-        }
-
-        public static int Modulus(int num1, int num2)
-        {
-            return num1 % num2;
-        }*/
-        #endregion
 
 
-        public static int Sum(params int[] nums)
-        {
-            var sum = 0;
-            foreach (int num in nums)
-            {
-                sum += num;
-            }
-            return sum;
-        }
 
 
 
